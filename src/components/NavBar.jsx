9e26a1546bad769f7b96ac69e80e30441d581000
AgentP324 <l20293982@gmail.com>
2024-12-30 22:55:52 +0800

    navabarv2

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,66 +11,66 @@ const Navbar = () => {
     };
     return (
         <>
-            <div className="m-0 p-0  flex bg-gray-400 ">
+            <div className="m-0 p-0  flex bg-color3">
                 <div className=" flex  p-10 rounded-xl ">
-                    <div className="bg-gray-300 p-4 rounded-xl w-[20vw] h-[95vh] " >
-                        <div className=" bg-gray-300 p-2  flex-col flex-wrap text-center ">
+                    <div className="bg-color2 p-4 rounded-xl w-[20vw] h-[95vh] " >
+                        <div className=" bg-color2 p-2  flex-col flex-wrap text-center ">
                             <button
-                                className={`rounded-3xl  text-white py-5 px-4 hover:bg-gray-300 text-3xl ${activeTab === '會員管理' ? 'bg-gray-500' : 'bg-gray-300'
+                                className={`rounded-3xl  text-black py-5 px-4 bg-color2 text-3xl ${activeTab === '主頁' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('主頁')}>
                                 <h1 className="text-4xl  font-bold text-gray-800 italic text-center font-poppins">Kagu machi</h1>
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '會員管理' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1   text-3xl ${activeTab === '會員管理' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('會員管理')}>
                                 會員管理
                             </button>
                             <button
-                                className={`rounded-3xl w-full  block  text-center text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '商品管理' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === '商品管理' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('商品管理')}>
                                 商品管理
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '訂單管理' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === '訂單管理' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('訂單管理')}>
                                 訂單管理
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '財務管理' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === '財務管理' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('財務管理')}>
                                 財務管理
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '庫存/倉儲' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === '庫存/倉儲' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('庫存/倉儲')}>
                                 庫存/倉儲
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '行銷/促銷' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === '行銷/促銷' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('行銷/促銷')}>
                                 行銷/促銷
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '數據/報表' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === '數據/報表' ?'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('數據/報表')}>
                                 數據/報表
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === ' 客服系統' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1  text-3xl ${activeTab === ' 客服系統' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('客服系統')}>
                                 客服系統
                             </button>
                             <button
-                                className={`rounded-3xl w-full block text-white py-5 px-4 hover:bg-gray-500 text-3xl ${activeTab === '設定' ? 'bg-gray-300' : 'bg-gray-300'
+                                className={`rounded-3xl w-full block text-black py-5 px-4 hover:text-white hover:bg-color1 text-3xl ${activeTab === '設定' ? 'bg-color2' : 'bg-color2'
                                     }`}
                                 onClick={() => openPage('設定')}>
                                 設定
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   };
   return (
     <>
-      <div className="w-full flex  p-10  bg-gray-400">
+      <div className="w-full flex  p-10  bg-color3">
         <div className=" p-4 rounded-xl h-[95vh] " >
           <img src={A} />
         </div>

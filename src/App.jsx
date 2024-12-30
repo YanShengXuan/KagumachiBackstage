c8a85eb33a464f5d66ba584c7ba7ba7e12ba3f91
YanShengXuan <shengxuan.yan1995@gmail.com>
2024-12-30 15:34:53 +0800

    未變更，改縮排

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import NavBar from './components/NavBar'
 import './App.css'
 
+import FinancialManagement from './pages/FinancialManagement.jsx'
+import OrderManagement from './pages/OrderManagement.jsx'
+
+
 function App() {
 
   return (

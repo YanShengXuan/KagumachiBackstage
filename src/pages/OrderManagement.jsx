3132d3bf2cbf2935bf22b5f03ff8ec1090be9822
YanShengXuan <shengxuan.yan1995@gmail.com>
2024-12-31 03:08:41 +0800

    版面調整

--- src/pages/OrderManagement.jsx
+++ src/pages/OrderManagement.jsx
@@ -7,11 +7,11 @@ function OrderManagement() {
   for (let i = 0; i < 20; i++) {
     rows.push(
       <tr key={i + 1}>
-        <td className="border-2 text-center text-4xl px-6">20241229001</td>
-        <td className="border-2 text-center text-4xl px-6">王小明</td>
-        <td className="border-2 text-center text-4xl px-6">2024-12-29</td>
-        <td className="border-2 text-center text-4xl px-6">30000</td>
-        <td className="border-2 text-center text-4xl px-6">配送中</td>
+        <td className="border-2 border-black text-center text-4xl px-6">20241229001</td>
+        <td className="border-2 border-black text-center text-4xl px-6">王小明</td>
+        <td className="border-2 border-black text-center text-4xl px-6">2024-12-29</td>
+        <td className="border-2 border-black text-center text-4xl px-6">30000</td>
+        <td className="border-2 border-black text-center text-4xl px-6">配送中</td>
       </tr>
     );
   }
@@ -19,8 +19,8 @@ function OrderManagement() {
   return (
     <>
       <div className="flex bg-gray-400 h-[100%]">
-        <div className="flex flex-col border-2 border-black  w-screen">
-          <div className="flex basis-1/4 border-2 border-black">
+        <div className="flex flex-col w-[100%]">
+          <div className="flex basis-1/4">
             <div className="flex-1 m-4">
               <OrderMonthBarChart />
             </div>
@@ -28,8 +28,8 @@ function OrderManagement() {
               <OrderCityBarChart />
             </div>
           </div>
-          <div className="basis-3/4 border-2 border-black h-48 p-2">
-            <div className="flex-row border-2 border-black h-full">
+          <div className="basis-3/4 h-48 p-2">
+            <div className="flex-row h-[100%]">
               <div className="flex justify-between basis-2/12">
                 <div className="flex">
                   <Date className="p-1" />
@@ -49,21 +49,21 @@ function OrderManagement() {
                   </button>
                 </div>
               </div>
-              <div className="flex basis-10/12 justify-center overflow-y-auto h-3/4 mt-8">
-                <table className="border-2 border-collapse">
+              <div className="flex basis-10/12 justify-center overflow-y-auto h-[80%] mt-4">
+                <table className="border-2 border-collapse w-[100%] mr-1">
                   <thead>
                     <tr>
-                      <th className="border-2 text-center text-4xl">
+                      <th className="border-2 border-black text-center text-4xl w-[20%]">
                         訂單編號
                       </th>
-                      <th className="border-2 text-center text-4xl">訂購人</th>
-                      <th className="border-2 text-center text-4xl">
+                      <th className="border-2 border-black text-center text-4xl w-[20%]">訂購人</th>
+                      <th className="border-2 border-black text-center text-4xl w-[20%]">
                         訂購日期
                       </th>
-                      <th className="border-2 text-center text-4xl">
+                      <th className="border-2 border-black text-center text-4xl w-[20%]">
                         訂購金額
                       </th>
-                      <th className="border-2 text-center text-4xl">
+                      <th className="border-2 border-black text-center text-4xl w-[20%]">
                         配送狀態
                       </th>
                     </tr>

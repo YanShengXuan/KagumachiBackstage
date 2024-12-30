ed9768b7279e0f676704a57a9940f69510745fb5
YanShengXuan <shengxuan.yan1995@gmail.com>
2024-12-31 03:09:02 +0800

    建立財務管理

--- src/pages/FinancialManagement.jsx
+++ src/pages/FinancialManagement.jsx
@@ -1,25 +1,45 @@
 import Date from "../components/Date.jsx";
 
 function FinancialManagement() {
+  const rows = [];
+  for (let i = 0; i < 20; i++) {
+    rows.push(
+      <tr key={i + 1}>
+        <td className="border-2 border-black text-center text-4xl px-6">2024-12-31</td>
+        <td className="border-2 border-black text-center text-4xl px-6">123</td>
+        <td className="border-2 border-black text-center text-4xl px-6">123</td>
+        <td className="border-2 border-black text-center text-4xl px-6">123</td>
+      </tr>
+    );
+  }
+
   return (
     <>
-      <div className="flex bg-gray-400 h-screen">
-        <div className="basis-1/4 border-2 border-black text-center">
-          我是選單
-        </div>
-        <div className="flex flex-col basis-3/4 border-2 border-black">
+      <div className="bg-color2 h-[100%] pt-10">
+        <div className="flex flex-col">
           <div className="flex">
             <Date />
             <button className="px-2 my-2 w-32 bg-amber-600 text-black rounded hover:bg-lime-600">
               確認
             </button>
-            <select className="w-[25%] h-[75%] mt-1.5 ml-2 text-center text-2xl" name="" id="">
-              <option value="">支出</option>
-              <option value="">收入</option>
-              <option value="">淨利</option>
-            </select>
           </div>
         </div>
+        <div className="flex justify-center overflow-y-auto h-[80%]">
+          <table className="border-2 border-collapse w-[100%]">
+            <thead>
+              <tr>
+                <th className="border-2 border-black text-center text-4xl w-[25%]">日期</th>
+                <th className="border-2 border-black text-center text-4xl w-[25%]">支出</th>
+                <th className="border-2 border-black text-center text-4xl w-[25%]">收入</th>
+                <th className="border-2 border-black text-center text-4xl w-[25%]">淨利</th>
+              </tr>
+            </thead>
+            {/* 測試內容 */}
+            <tbody>{rows}</tbody>
+            {/* 測試內容 */}
+          </table>
+        </div>
+        <div className="text-right text-4xl pr-4">小計:10</div>
       </div>
     </>
   );
